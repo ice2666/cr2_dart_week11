1c7aec1a91b03be92f3e1f8c5f8f87c1d648b7ee
rathapoom260448 <rathaphumiwicitrsmbati.890@gmail.com>
2022-07-25 11:39:54 +0700

    first commit

---
+++ bin/forloop.dart
@@ -0,0 +1,6 @@
+void main () {
+  int i = 1;
+  for (i; i <= 12; i = i + 1) {
+    print('2 × $i = ${i * 2}');
+  }
+}
---
+++ bin/forloop2.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main () {
+  print("👉🏻Enter number: 💥");
+  int x = int.parse(stdin.readLineSync()!);
+  int i = 1;
+  for (i; i <=12; i = i + 1) {
+    print('💢🥺$x x $i = ${i * x}💗');
+  }
+}
